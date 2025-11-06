44fcf9575ab5bc10d099daec6fe87464e198d09b
Sicking10 <riosnavarretejared@gmail.com>
2025-11-06 00:08:35 -0700

    Diseño mejorado en index.html, se implemento la metodologia BEM y semantica HTML, el Login fue mejorado

--- passportConfig.js
+++ passportConfig.js
@@ -1,3 +1,4 @@
+// Inicio de sesion de google
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('./public/user'); // tu modelo de usuario
